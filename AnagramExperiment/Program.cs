e28b7ed135a3d3ed56403379d97145ca722676f5
the f <feldrim@gmail.com>
2017-11-03 23:07:33 +0300

    Modified program.cs
    Added sample console application that uses args as path and word parameter and finds anagrams.

--- AnagramExperiment/Program.cs
+++ AnagramExperiment/Program.cs
@@ -1,14 +1,38 @@
-﻿namespace AnagramExperiment
+﻿using System;
+using System.Diagnostics;
+
+namespace AnagramExperiment
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            var path = args[0];
-            var word = args[1];
+            //var path = args[0];
+            //var word = args[1];
 
+            const string path = @"..\..\..\AnagramExperiment\Data\Sample.txt";
+            const string word = "emits";
+            var stopwatch = new Stopwatch();
+
+            Console.Write("Creating Anagram Dictionary...\t");
+            stopwatch.Start();
             var dictionary = new AnagramDictionary(path);
-            dictionary.LookUpWord(word);
+            stopwatch.Stop();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+
+            Console.Write($"Looking up anagrams of {word}...\t");
+            stopwatch.Restart();
+            var anagrams = dictionary.LookUpWord(word);
+            stopwatch.Stop();
+            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
+
+            foreach (var anagram in anagrams)
+            {
+                Console.WriteLine(anagram);
+            }
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
