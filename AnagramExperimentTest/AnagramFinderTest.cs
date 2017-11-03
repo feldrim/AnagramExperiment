fa211f1a9f7fa2e1b62e765e310d11085b5e2234
the f <feldrim@gmail.com>
2017-11-03 18:04:29 +0300

    Test Project Added
    New Unit Test project added.

---
+++ AnagramExperimentTest/AnagramFinderTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AnagramExperimentTest
+{
+    [TestClass]
+    public class AnagramFinderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
