bcc83bbda880bd9612c0622633ef65e7ece52080
the f <feldrim@gmail.com>
2017-11-03 18:10:22 +0300

    Test Initialize
    Added properties and TestInitialize method.

--- AnagramExperimentTest/AnagramFinderTest.cs
+++ AnagramExperimentTest/AnagramFinderTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AnagramExperimentTest
@@ -6,6 +7,19 @@ namespace AnagramExperimentTest
     [TestClass]
     public class AnagramFinderTest
     {
+        public AnagramExperiment.AnagramDictionary TestDictionary;
+        public AnagramExperiment.AnagramFinder AnagramFinderTestInstance;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            TestDictionary = new AnagramDictionary();
+            TestDictionary.Add("eimst", new List<string> {"emits", "smite"});
+
+            AnagramFinderTestInstance = new AnagramFinder();
+
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
