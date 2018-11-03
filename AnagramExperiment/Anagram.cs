9a1f0bf513cf4b7bc60653ac3b3e5ce8e300464e
the f <feldrim@gmail.com>
2018-11-03 22:15:42 +0300

    Added "Profiler" class for method run time measuring.

--- AnagramExperiment/Anagram.cs
+++ AnagramExperiment/Anagram.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 
 namespace AnagramExperiment
@@ -15,25 +14,23 @@ namespace AnagramExperiment
             var path = args[0];
             var word = args[1];
 
-            var stopwatch = new Stopwatch();
+           using (var profiler = new Profiler("Anagram experiment"))
+           {
+              var dictionary = AnagramDictionary.Create(path);
+              profiler.Print("Creating anagram dictionary");
+              
+              var anagrams = dictionary.LookUpWord(word);
+              profiler.Print($"Looking up anagrams of {word}");
 
-            Console.Write("Creating Anagram Dictionary...\t");
-            stopwatch.Start();
-            var dictionary = AnagramDictionary.Create(path);
-            stopwatch.Stop();
-            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
-
-            Console.Write($"Looking up anagrams of '{word}'...\t");
-            stopwatch.Restart();
-            var anagrams = dictionary.LookUpWord(word);
-            stopwatch.Stop();
-            Console.WriteLine($"{stopwatch.ElapsedMilliseconds} ms");
-
-            if (anagrams.Any())
-                foreach (var anagram in anagrams)
+              Console.WriteLine("Anagrams:");
+              if (anagrams.Any())
+                 foreach (var anagram in anagrams)
                     Console.WriteLine($"- {anagram}");
-            else
-                Console.WriteLine("No anagram found.");
+              else
+                 Console.WriteLine("No anagram found.");
+              profiler.Print("Printing anagrams");
+
+           }
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
---
+++ AnagramExperiment/Profiler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AnagramExperiment
+{
+   /// <inheritdoc />
+   public class Profiler : IDisposable
+   {
+      private bool _disposed;
+      private Stopwatch _stopWatch;
+      private long _elapsed;
+
+      public Profiler() : this("Untitled")
+      {
+      }
+
+      public Profiler(string title)
+      {
+         Console.WriteLine(title);
+         _stopWatch = new Stopwatch();
+         _stopWatch.Start();
+         _elapsed = 0;
+      }
+
+      public void Dispose()
+      {
+         Dispose(true);
+         GC.SuppressFinalize(this);
+      }
+
+      public void Print(string comment)
+      {
+         if (_disposed) throw new ObjectDisposedException(GetType().Name);
+         _elapsed += _stopWatch.ElapsedMilliseconds;      
+         Console.WriteLine("  {0,-30} {1,10} ms", Crop(comment, 30), _stopWatch.ElapsedMilliseconds);
+         _stopWatch.Restart();
+      }
+
+      protected virtual void Dispose(bool disposing)
+      {
+         if (_disposed) return;
+
+         _stopWatch.Stop();
+         _elapsed += _stopWatch.ElapsedMilliseconds;
+
+         Console.WriteLine("{0,-30}   {1,10} ms", "Total", _elapsed);
+         _stopWatch = null;
+
+         _disposed = true;
+      }
+
+      private static string Crop(string input, int length)
+      {
+         if (input.Length > length)
+            return input.Substring(0, length - 3) + "...";
+         return input;
+      }
+   }
+}
